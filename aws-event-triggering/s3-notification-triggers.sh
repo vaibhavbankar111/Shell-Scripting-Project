a56c99ec56b330b7293370af76a513542fdc322a
vaibhavbankar111 <137535357+vaibhavbankar111@users.noreply.github.com>
2023-06-29 17:06:00 +0530

    Update s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -9,11 +9,11 @@ aws_account_id=$(aws sts get-caller-identity --query 'Account' --output text)
 echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
-aws_region="ap-south-1"
-bucket_name="pawan-s3-event"
+aws_region="us-east-1"
+bucket_name="vaibhav-s3-event"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
-email_address="pawankahurke@outlook.com"
+email_address="vaibhavb2023@outlook.com"
 
 # Create IAM Role for the project
 role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
@@ -104,6 +104,6 @@ aws sns subscribe \
 aws sns publish \
   --topic-arn "$topic_arn" \
   --subject "A new object created in s3 bucket" \
-  --message "Hello from Pawan Kahurke"
+  --message "Hello from Vaibhav Bankar"
 
 
